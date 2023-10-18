811cc9ae42dbf8fa01fc4667ea0a3a89d6a77168
nilsv <nils.vogt@familyvogt.de>
2023-10-18 16:40:21 +0200

    2.2 in Bearbeitung

---
+++ src/org/hbrs/se1/ws23/uebung2/ConcreteMember.java
@@ -0,0 +1,14 @@
+package org.hbrs.se1.ws23.uebung2;
+
+public class ConcreteMember implements Member {
+  Integer id;
+  public ConcreteMember(Integer id) {
+    this.id = id;
+  }
+  public Integer getID() {
+    return this.id;
+  }
+  public String toString() {
+    return "Member (ID = " + id + ")";
+  }
+}
