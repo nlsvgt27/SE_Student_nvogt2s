e4380ce72147e3f7443dff03511f83adec805d13
nilsv <nils.vogt@familyvogt.de>
2023-10-25 13:43:04 +0200

    Start 3.2

---
+++ src/org/hbrs/se1/ws23/uebung3/Container/Container.java
@@ -0,0 +1,40 @@
+package org.hbrs.se1.ws23.uebung3.Container;
+
+import org.hbrs.se1.ws23.uebung2.Member.Member;
+
+import java.util.ArrayList;
+
+public class Container {
+  ArrayList<Member> list = new ArrayList<>();
+
+  public int size() {
+    return list.size();
+  }
+
+  public void addMember(Member member) throws ContainerException {
+    for (Member e: list) {
+      if (e.getID().equals(member.getID())) {
+        throw new ContainerException("Das Member-Objekt mit der ID " + member.getID() + " ist bereits vorhanden!");
+      }
+    }
+    list.add(member);
+  }
+
+  public String deleteMember(Integer id) {
+    for (int i = 0; i < list.size(); i++) {
+      if(list.get(i).getID().equals(id)) {
+        list.remove(i);
+        return "";
+      }
+    }
+
+    return "Es wurde kein Member mit der von Inhnen gesuchten ID gefunden, und konnte deshalb auch nicht gelöscht werden";
+  }
+
+  public void dumb() {
+    for (Member e: list) {
+      System.out.println(e.toString());
+    }
+  }
+
+}
---
+++ src/org/hbrs/se1/ws23/uebung3/Container/ContainerException.java
@@ -0,0 +1,8 @@
+package org.hbrs.se1.ws23.uebung3.Container;
+
+public class ContainerException extends Exception {
+
+  public ContainerException(String t) {
+    System.out.println(t);
+  }
+}
