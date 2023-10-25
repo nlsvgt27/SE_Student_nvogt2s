94b74d98f90bb065bdbb879509618e26d79e28f3
saschaalda <sascha.alda@h-brs.de>
2023-10-25 13:23:46 +0200

    Upload Codes für Übung Nr. 3

---
+++ src/org/hbrs/se1/ws23/uebung3/persistence/PersistenceStrategyMongoDB.java
@@ -0,0 +1,27 @@
+package org.hbrs.se1.ws23.uebung3.persistence;
+
+import java.util.List;
+
+public class PersistenceStrategyMongoDB<E> implements PersistenceStrategy<E> {
+
+    @Override
+    public void openConnection() throws PersistenceException {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    @Override
+    public void closeConnection() throws PersistenceException {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    @Override
+    public void save(List<E> member) {
+        throw new UnsupportedOperationException("Not implemented!");
+
+    }
+
+    @Override
+    public List<E> load() {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+}
