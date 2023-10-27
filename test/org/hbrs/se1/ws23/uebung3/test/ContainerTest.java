3565de73823c27dc01131390d9ecbcc0a8c2fd52
nilsv <nils.vogt@familyvogt.de>
2023-10-27 11:58:09 +0200

    Bearbeitung 3.2 V8: erstellung von ContainerTest

---
+++ test/org/hbrs/se1/ws23/uebung3/test/ContainerTest.java
@@ -0,0 +1,11 @@
+package org.hbrs.se1.ws23.uebung3.test;
+
+import org.hbrs.se1.ws23.uebung3.Container.Container;
+import org.hbrs.se1.ws23.uebung3.Container.ContainerException;
+import org.hbrs.se1.ws23.uebung2.Member.ConcreteMember;
+import org.hbrs.se1.ws23.uebung2.Member.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ContainerTest {
+}
