b7191c8e68adc1da3f1058a62bb050ea95388f7b
saschaalda <sascha.alda@h-brs.de>
2023-11-15 18:42:54 +0100

    Verbesserungen der Lösung von Julius P., nach Besprechung in Übung. Eine weitere Aufteilung der Klasse Container ist natürlich notwendig, siehe Klassendiagramm zur Aufgabe 4-2 auf LEA. Eine erste Struktur der Packages ist hier ebenfalls angedeutet

---
+++ src/org/hbrs/se1/ws23/uebung4/prototype/improvements/controller/ContainerException.java
@@ -0,0 +1,29 @@
+package org.hbrs.se1.ws23.uebung4.prototype.improvements.controller;
+
+public class ContainerException extends Exception {
+	
+	private String modus;
+	private Integer id;
+	
+	public ContainerException( String s ) {
+		super ( s );
+	}
+
+	/**
+	 * Einfaches Exception-Handling. Orientierung an die Lösung Übungsaufgabe Nr. 3-2 besser...
+	 */
+	@Override
+	public void printStackTrace() {
+		if (this.id  != null) {
+			System.out.println("Das Person-Objekt mit der ID " + this.id + " ist bereits vorhanden!");
+		} else {
+			System.out.println(this.getMessage());
+		}
+	} 
+
+	public void addID(Integer id) {
+		this.id = id;
+	}
+
+
+}
---
+++ src/org/hbrs/se1/ws23/uebung4/prototype/improvements/controller/UserStory.java
@@ -0,0 +1,102 @@
+package org.hbrs.se1.ws23.uebung4.prototype.improvements.controller;
+
+import java.io.Serializable;
+
+    public class UserStory implements Comparable<UserStory>, Serializable {
+
+        String titel;
+        int aufwand = 0;
+        int id = 0;
+        int mehrwert = 0;
+        int risk = 0;
+        int strafe = 0;
+        double prio = 0.0;
+
+        public String getProject() {
+            return project;
+        }
+
+        public void setProject(String project) {
+            this.project = project;
+        }
+
+        String project;
+
+
+        public UserStory(int id, String titel, int mehrwert, int strafe,
+                         int aufwand, int risk, double prio) {
+            this.id = id;
+            this.titel = titel;
+            this.mehrwert = mehrwert;
+            this.strafe = strafe;
+            this.aufwand = aufwand;
+            this.risk = risk;
+            this.prio = prio;
+        }
+
+        public UserStory() {
+        }
+
+        public double getPrio() {
+            return prio;
+        }
+
+        public void setPrio(double prio) {
+            this.prio = prio;
+        }
+
+        public String getTitel() {
+            return titel;
+        }
+        public void setTitel(String titel) {
+            this.titel = titel;
+        }
+        public int getAufwand() {
+            return aufwand;
+        }
+        public void setAufwand(int aufwand) {
+            this.aufwand = aufwand;
+        }
+        public int getId() {
+            return id;
+        }
+        public void setId(int id) {
+            this.id = id;
+        }
+        public int getMehrwert() {
+            return mehrwert;
+        }
+        public void setMehrwert(int mehrwert) {
+            this.mehrwert = mehrwert;
+        }
+        public int getRisk() {
+            return risk;
+        }
+        public void setRisk(int risk) {
+            this.risk = risk;
+        }
+        public int getStrafe() {
+            return strafe;
+        }
+        public void setStrafe(int strafe) {
+            this.strafe = strafe;
+        }
+
+        /*
+         * Methode zum Vergleich zweier UserStories.
+         * Vergleich ist implementiert auf Basis des Vergleichs
+         * von zwei Prio-Werten.
+         */
+        public int compareTo(UserStory input) {
+            if ( input.getPrio() == this.getPrio() ) {
+                return 0;
+            }
+
+            if ( input.getPrio() > this.getPrio() ) {
+                return 1;
+            }
+            else return -1;
+        }
+
+    }
+
